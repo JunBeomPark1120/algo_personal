0336800c80a4ce409ff7f9a47fafa445da16ddda
parkjunbeom <a01077348062@gmail.com>
2023-08-05 17:49:03 +0900

    자료구조 스택, 큐, 우선순위 큐 구현

---
+++ Implementation/Stack/my_stack.py
@@ -0,0 +1,15 @@
+# Python 스택 구현
+stack = []
+
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack[::-1]) # 최 상단 원소부터 출력하는 방식
+print(stack) # 최 하단 원소부터 출력
